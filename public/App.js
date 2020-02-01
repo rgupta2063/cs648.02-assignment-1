97c9f900d7ae044a36605de73f6e01ffe5235a4f
Ritu Gupta <ritugupta@Ghanshyams-MacBook-Air.local>
2020-01-31 16:31:33 -0800

    changed className

--- public/App.js
+++ public/App.js
@@ -1,8 +1,8 @@
 const element = React.createElement("div", {
-  class: "main",
+  className: "main",
   title: "outer div"
 }, React.createElement("div", {
-  class: "name"
+  className: "name"
 }, "Ritu Gupta"), React.createElement("img", {
   src: "image/myimage.jpg"
 }), React.createElement("p", null, " Hi,my name is Ritu Gupta and I am seeking MS(CS) in San Diego State University. I hail from India and have been living in USA since three years with my husband. My hobbies are painting,traveling and exploring new places around.But recently, I discovered that I am a good dancer too! I love whatever I do as I have a steady source of motivation that drives me to do my best. "), React.createElement("form", {
@@ -10,7 +10,7 @@ const element = React.createElement("div", {
   method: "get",
   target: "_blank"
 }, React.createElement("button", {
-  class: "button",
+  className: "button",
   type: "submit"
 }, "VIEW MY GITHUB REPO")));
 ReactDOM.render(element, document.getElementById('content'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 const element = (
-    <div class='main' title="outer div">
-        <div class='name'>Ritu Gupta</div>
+    <div className='main' title="outer div">
+        <div className='name'>Ritu Gupta</div>
         <img src="image/myimage.jpg"></img>
         <p> Hi,my name is Ritu Gupta and I am seeking MS(CS) in San Diego State University. I hail from India and have been living in USA since three years with my husband. My hobbies are painting,traveling and exploring new places around.But recently, I discovered that I am a good dancer too! I love whatever I do as I have a steady source of motivation that drives me to do my best. </p>
         <form action="https://github.com/rgupta2063/cs648.02-assignment-1.git" method="get" target="_blank">
-            <button class='button' type="submit">VIEW MY GITHUB REPO</button>
+            <button className='button' type="submit">VIEW MY GITHUB REPO</button>
         </form>
     </div>);
 
